fecc2233594cfa449dc1ecb2d4f7361925817a39
Erasyn <wyll.brimacombe@gmail.com>
2020-05-26 18:29:27 -0700

    three js init

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+class Container extends React.Component {
+  state = { isMounted: true };
+
+  render() {
+    const { isMounted = true } = this.state;
+    return (
+      <>
+        <button onClick={() => this.setState(state => ({ isMounted: !state.isMounted }))}>
+          {isMounted ? "Unmount" : "Mount"}
+        </button>
+        {isMounted && <App />}
+        {isMounted && <div>Scroll to zoom, drag to rotate</div>}
+      </>
+    )
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Container />, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
