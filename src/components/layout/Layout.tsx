dc3b58db01d7919b2d2f5861bae0a16250c3a14d
Wyll Brimacombe <wyll.brimacombe@gmail.com>
2023-10-11 18:32:01 +0100

    update container styling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from "react";
 import Desktop from "./Desktop";
 import Taskbar from "./Taskbar";
 
-export default function Layout({ children }: any) {
+export default function Layout() {
   return (
-    <>
+    <div className=" flex min-h-screen min-w-full flex-col justify-end bg-sky-700">
       <Desktop />
       <Taskbar />
-    </>
+    </div>
   );
 }
--- src/components/layout/Taskbar.tsx
+++ src/components/layout/Taskbar.tsx
@@ -3,11 +3,16 @@ import NavItem from "../navigation/NavItem";
 
 export default function Taskbar() {
   return (
-    <div className="taskbar flex items-center">
+    <div className="taskbar flex items-center justify-between bg-stone-300">
       <NavBar>
         <NavItem>START</NavItem>
         <NavItem url={"about"}>About</NavItem>
       </NavBar>
+      <Widget />
     </div>
   );
+}
+
+function Widget() {
+  return <div className=" m-2">WIDGET</div>;
 }
